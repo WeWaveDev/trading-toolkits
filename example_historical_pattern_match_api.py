698f5e6af304caeead66a27114080d89d5529e43
aj <aj@liptalk.io>
2023-12-04 14:17:34 -0800

    added plotting

--- example_historical_pattern_match_api.py
+++ example_historical_pattern_match_api.py
@@ -1,7 +1,88 @@
 import datetime
 import os
 
+import matplotlib.pyplot as plt
+import numpy as np
 import requests
+
+
+def plot_pattern_match_and_prediction(matched_event_candles, matched_event_datapoints):
+    # Extracting time and closing values
+    times = [candle['t'] for candle in matched_event_candles]
+    closing_values = [candle['c'] for candle in matched_event_candles]
+
+    # Plotting the lines with different colors
+    plt.figure(figsize=(10, 5))
+    plt.plot(times[:matched_event_datapoints], closing_values[:matched_event_datapoints], color='blue', label='Matched Zone')
+    plt.plot(times[matched_event_datapoints:], closing_values[matched_event_datapoints:], color='red', label='Prediction Zone')
+    plt.xlabel('Time')
+    plt.ylabel('Closing Value')
+    plt.title('Matched Zone vs Prediction Zone')
+    plt.legend()
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+    
+def plot_all_matches(data_array):
+    # Adjusting the script to handle 20 subcharts of matched_event_candles
+
+    # Generating sample data for 20 subcharts
+    subcharts_data = []
+    for i in range(20):
+        subchart = [{'t': f'2023-01-{i+1:02d}', 'c': 50 + i*2 + j} for j in range(8)]
+        subcharts_data.append(subchart)
+
+    # Defining matched_event_datapoints for each subchart
+    matched_event_datapoints = 5
+
+    # Plotting 20 subcharts
+    fig, axs = plt.subplots(5, 4, figsize=(20, 15), constrained_layout=True)
+
+    for i, ax in enumerate(axs.flatten()):
+        if i < len(subcharts_data):
+            times = [candle['t'] for candle in subcharts_data[i]]
+            closing_values = [candle['c'] for candle in subcharts_data[i]]
+
+            ax.plot(times[:matched_event_datapoints], closing_values[:matched_event_datapoints], color='blue')
+            ax.plot(times[matched_event_datapoints:], closing_values[matched_event_datapoints:], color='red')
+            ax.set_title(f'Subchart {i+1}')
+            ax.tick_params(axis='x', rotation=45)
+
+    plt.suptitle('Matched Event Candles: Matched Zone vs Prediction Zone in Subcharts')
+    plt.show()
+
+def create_subplots(data_array):
+    # Determine the number of rows and columns for subplots
+    num_subcharts = len(data_array)
+    rows = int(np.ceil(num_subcharts / 4))
+    cols = min(num_subcharts, 4)
+
+    fig, axs = plt.subplots(rows, cols, figsize=(20, 5 * rows), constrained_layout=True)
+
+    # Flatten the axes array for easy indexing
+    axs = axs.flatten()
+
+    for i, ax in enumerate(axs):
+        if i < num_subcharts:
+            subchart_data = data_array[i]
+            matched_event_candles = subchart_data['matched_event_candles']
+            matched_event_datapoints = subchart_data['matched_event_datapoints']
+            
+            times = [candle['t'] for candle in matched_event_candles]
+            closing_values = [candle['c'] for candle in matched_event_candles]
+
+            ax.plot(times[:matched_event_datapoints], closing_values[:matched_event_datapoints], color='blue')
+            ax.plot(times[matched_event_datapoints:], closing_values[matched_event_datapoints:], color='red')
+            ax.set_title(f'Subchart {i+1}: {subchart_data["matched_start_date_string"]} to {subchart_data["matched_end_date_string"]}')
+            ax.tick_params(axis='x', rotation=45)
+        else:
+            # Hide unused subplots
+            ax.axis('off')
+
+    plt.suptitle('Matched Event Candles: Matched Zone vs Prediction Zone in Subcharts')
+    plt.show()
+
+
 
 if __name__ == '__main__':
     url = r"https://j32e1smuxe.execute-api.us-east-1.amazonaws.com/prod/api/v1/dynamic-history-match/"
@@ -38,7 +119,9 @@ if __name__ == '__main__':
     # for key, value in json_response['data']:
     #     print(key)
 
-    matched_event_array_top = matched_event_array[:3]
+    matched_event_array_top = matched_event_array[:20] # NOTE: consider the first 20 results
+    
+    storage_for_plotting = []
 
     for index, matched_event in enumerate(matched_event_array_top):
         print('matched event #{}'.format(index))
@@ -77,8 +160,19 @@ if __name__ == '__main__':
         for index, candle in enumerate(matched_event_candles[matched_event_datapoints:]):
             print(index, candle.get('c'))
         
-            
+        storage_for_plotting.append(
+            {
+                'matched_event_candles': matched_event_candles,
+                'matched_event_datapoints': matched_event_datapoints,
+                'matched_start_date_string': matched_start_date_string,
+                'matched_end_date_string': matched_end_date_string,
+            }
+        )    
         print('---------------------------------')
+        
+        # plot_pattern_match_and_prediction(matched_event_candles, matched_event_datapoints)
+    create_subplots(storage_for_plotting)
+        
             
             
     
