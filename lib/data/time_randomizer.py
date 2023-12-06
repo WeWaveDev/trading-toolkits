f76a1c691c4b529bacc239d3a0dd0e689e0569a8
aj <aj@liptalk.io>
2023-12-06 13:18:39 -0800

    added parameter to time randomizer

--- lib/data/time_randomizer.py
+++ lib/data/time_randomizer.py
@@ -2,17 +2,22 @@ import datetime
 import random
 
 
-def get_randomized_start_and_end_date_dict():
-    # Randomize a date between 2000-01-01 and 2023-11-01
-    start_date = datetime.date(1980, 1, 1)
-    end_date = datetime.date(2023, 11, 1)
+def get_randomized_start_and_end_date_dict(
+    start_time_string='1980-01-01',
+    end_time_string='2023-11-01',
+    random_days_min=20,
+    random_days_max=200,
+):
+    # Convert string to datetime
+    start_date = datetime.datetime.strptime(start_time_string, '%Y-%m-%d')
+    end_date = datetime.datetime.strptime(end_time_string, '%Y-%m-%d')
     time_between_dates = end_date - start_date
     days_between_dates = time_between_dates.days
     random_number_of_days = random.randrange(days_between_dates)
     random_date = start_date + datetime.timedelta(days=random_number_of_days)
 
     # Randomize a number between 20 and 200
-    random_days = random.randint(20, 200)
+    random_days = random.randint(random_days_min, random_days_max)
 
     # Calculate end_time_to_match_pattern and start_time_to_match_pattern
     end_time_to_match_pattern = random_date
@@ -21,7 +26,7 @@ def get_randomized_start_and_end_date_dict():
     # Display the results
     print("Start Time to Match Pattern: ", start_time_to_match_pattern)
     print("End Time to Match Pattern: ", end_time_to_match_pattern)
-    print("random_days: ", random_days)
+    print("random_days for calendar days ", random_days)
     
     # # convert from datetime to string
     # start_time_to_match_pattern = start_time_to_match_pattern.strftime('%Y-%m-%d')
