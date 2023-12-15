8a1c25715def9ccf8ad757c3f00d7fed0b1b366f
aj <aj@liptalk.io>
2023-12-15 15:59:02 -0800

    change size; add stop loss

--- 14_example_backtesting_using_historical_pattern_match_limited_order_spy.py
+++ 14_example_backtesting_using_historical_pattern_match_limited_order_spy.py
@@ -12,23 +12,23 @@ from lib.data.pattern_match import (
 symbol = 'SPY'
 time_interval = '1D'
 # Fetching the OHLC data
-ohlc_history_in_dataframe = get_ohlc_history_in_dataframe(symbol, time_interval, '2020-01-01', '2021-01-01')
+ohlc_history_in_dataframe = get_ohlc_history_in_dataframe(symbol, time_interval, '2022-01-01', '2023-01-01')
 
+CALL_HISTORICAL_PATTERN_MATCH_EVERY_N_TRADING_DAYS = 5
 
 date_to_object_dict = {}
 
 def signal_generator(arr: pd.DatetimeIndex) -> pd.Series:
-    print('debug arr', arr  )
     # Create an empty list to store the signals
     signals = []
 
     # Iterate through each date in the series
     for index, date in enumerate(arr):
 
-        if index % 10 == 0: # project every 10 trading days
+        if index % CALL_HISTORICAL_PATTERN_MATCH_EVERY_N_TRADING_DAYS == 0: # project every N trading days
             # start_time_millisecond  = date -minus 60 days
-            start_time_millisecond = date.timestamp() * 1000 - 60 * 24 * 60 * 60 * 1000 
-            end_time_millisecond = date.timestamp() * 1000
+            start_time_millisecond = date.timestamp() * 1000 - (1+60) * 24 * 60 * 60 * 1000 
+            end_time_millisecond = date.timestamp() * 1000 - 1  * 24 * 60 * 60 * 1000 # end date -1 is because to simultae the real case that not letting the api know today's close price to avoid look ahead bias
             print('call WeWave historical pattern match', date, start_time_millisecond, end_time_millisecond)
             matched_event_array = call_historcial_pattern_match_by_milliseconds(
                 symbol,
@@ -42,16 +42,16 @@ def signal_generator(arr: pd.DatetimeIndex) -> pd.Series:
                 signals.append(1)
                 date_to_object_dict[date] ={
                     'direction': 1,
-                    'take_profit_perc': sumarized['mean_return'] ,
-                    # 'stop_loss_perc': sumarized['mean_return'] * 0.5,
+                    'take_profit_perc': sumarized['mean_return'] *2,
+                    'stop_loss_perc': sumarized['mean_return'],
                     'limit_perc': (sumarized['min_return'] + sumarized['mean_return']) /2
                 }
             elif sumarized['mean_return'] < -0.3:
                 signals.append(-1)
                 date_to_object_dict[date] = {
                         'direction': -1,
-                        'take_profit_perc': -1*(sumarized['mean_return']), # make it positive
-                        # 'stop_loss_perc': sumarized['mean_return'] * 0.5,
+                        'take_profit_perc': -1*(sumarized['mean_return'])*2, # make it positive
+                        'stop_loss_perc':  -1* sumarized['mean_return'], # make it positive
                         'limit_perc': (sumarized['max_return'] + sumarized['mean_return']) /2
                     }
                 
@@ -88,28 +88,28 @@ class ProfitTargetStrategy(Strategy):
         
         if self.signal[-1] == 1:
             take_profit = (1+signal_object['take_profit_perc']*0.01) * current_price 
-            # stop_loss = (1-signal_object['stop_loss_perc']*0.01) * current_price 
+            stop_loss = (1-signal_object['stop_loss_perc']*0.01) * current_price 
             # limit = (1+signal_object['limit_perc']*0.01) * current_price
             limit = None
             print('debug buy', current_price, take_profit, limit)
             self.buy(
-                # size=0.2, 
+                size=0.2, 
                 #  limit=limit,
                 tp=take_profit, 
-                #sl=stop_loss
+                sl=stop_loss
             ) 
 
         if self.signal[-1] == -1:
             take_profit = (1-signal_object['take_profit_perc']*0.01) * current_price 
-            # stop_loss = (1+signal_object['stop_loss_perc']*0.01) * current_price 
+            stop_loss = (1+signal_object['stop_loss_perc']*0.01) * current_price 
             # limit = (1+signal_object['limit_perc']*0.01) * current_price
             limit = None
             print('debug sell', current_price, take_profit, limit, signal_object['take_profit_perc'])
             self.sell(
-                # size=0.2, 
+                size=0.2, 
                 #   limit=limit, 
                 tp=take_profit, 
-                #sl=stop_loss
+                sl=stop_loss
             ) 
                         
             
