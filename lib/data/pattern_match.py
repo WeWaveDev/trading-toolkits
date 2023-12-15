8ab783b0a8f331e1127ca7ec445d75763345edf1
aj <aj@liptalk.io>
2023-12-14 17:42:28 -0800

    add summarize_matched_event_array

--- lib/data/pattern_match.py
+++ lib/data/pattern_match.py
@@ -70,3 +70,59 @@ def call_historcial_pattern_match(
         end_time_to_match_pattern_timestamp_milliseconds,
         timeInterval
     )
+    
+    
+    
+def summarize_matched_event_array(matched_event_array):
+    
+    top_events_to_consider = 20
+    
+    # sort by score
+    sorted_matched_event_array = sorted(matched_event_array, key=lambda x: x['score'], reverse=True)
+    
+    # only consider top 20 events
+    matched_event_array = sorted_matched_event_array[:top_events_to_consider]
+    
+    # calculate mean return
+    mean_return = 0
+    weighted_mean_return = 0 # the later in the event less weighted
+    max_return = -float('inf')  # Set to negative infinity for initialization
+    min_return = float('inf')  # Set to positive infinity for initialization
+    total_weight = 0
+
+    
+    for event in matched_event_array:
+        matched_event_candles = event['candles']        
+        matched_event_datapoints = event['dataPoints']
+        closing_values = [candle['c'] for candle in matched_event_candles]
+        last_close_in_match_zone = closing_values[matched_event_datapoints-1] # it is a price
+        # projected_return_percentage_array is an array that closing_values[matched_event_datapoints:] / last_close_in_match_zone
+        
+        if last_close_in_match_zone <= 0:
+            continue
+        projected_return_percentage_array = [ 100*(close -last_close_in_match_zone) / last_close_in_match_zone for close in closing_values[matched_event_datapoints:] ]
+        
+        event_mean_return = sum(projected_return_percentage_array) / len(projected_return_percentage_array)
+        mean_return += event_mean_return
+
+        # Weighted mean calculation (later events less weighted)
+        weight = 1
+        weighted_mean_return += event_mean_return * weight
+        total_weight += weight
+
+        # Update max and min returns
+        max_return = max(max_return, max(projected_return_percentage_array, default=max_return))
+        min_return = min(min_return, min(projected_return_percentage_array, default=min_return))
+
+    # Finalize mean and weighted mean calculations
+    mean_return /= len(matched_event_array)
+    weighted_mean_return /= total_weight if total_weight else 1  # Avoid division by zero
+
+    
+    return {
+        'number_of_events': len(matched_event_array),
+        'mean_return': mean_return,
+        'weighted_mean_return': weighted_mean_return,
+        'max_return': max_return,
+        'min_return': min_return
+    }
